7ed68552aa4fcd5f04bdf79f3064de68818d6585
pawel34 <33285136-pawel34@users.noreply.replit.com>
2025-01-28 15:29:43 +0000

    Agent query: Czy aplikacja działa poprawnie po dodaniu nowych kluczy CMS dla pól formularza?
    Separate field labels and descriptions in CMS keys and form sections.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/eb7d8b7e-5991-410c-803c-7fa2f4a70cf3/7103e4e4-964b-422d-a257-2ef9fcf196e9.jpg

--- client/src/lib/cms-keys.ts
+++ client/src/lib/cms-keys.ts
@@ -4,40 +4,66 @@ export const formCmsKeys = {
       title: "sleep_parameters_section_title",
       description: "sleep_parameters_section_description",
       fields: {
-        sleepQuality: "sleep_quality_field_description",
-        sleptWithWindowOpen: "slept_with_window_open_field_description",
-        hadGoodDreams: "had_good_dreams_field_description",
-        hadBadDreams: "had_bad_dreams_field_description",
-        wokeUpToToilet: "woke_up_to_toilet_field_description",
-        sleepTime: "sleep_time_field_description",
-        wakeTime: "wake_time_field_description",
-        neckStiffness: "neck_stiffness_field_description",
-        timeToGetUp: "time_to_get_up_field_description"
+        sleepQuality: "sleep_quality_field_label",
+        sleepQualityDesc: "sleep_quality_field_description",
+        totalSleepDuration: "total_sleep_duration_field_label",
+        totalSleepDurationDesc: "total_sleep_duration_field_description",
+        deepSleepDuration: "deep_sleep_duration_field_label",
+        deepSleepDurationDesc: "deep_sleep_duration_field_description",
+        sleptWithWindowOpen: "slept_with_window_open_field_label",
+        sleptWithWindowOpenDesc: "slept_with_window_open_field_description",
+        hadGoodDreams: "had_good_dreams_field_label",
+        hadGoodDreamsDesc: "had_good_dreams_field_description",
+        hadBadDreams: "had_bad_dreams_field_label",
+        hadBadDreamsDesc: "had_bad_dreams_field_description",
+        wokeUpToToilet: "woke_up_to_toilet_field_label",
+        wokeUpToToiletDesc: "woke_up_to_toilet_field_description",
+        sleepTime: "sleep_time_field_label",
+        sleepTimeDesc: "sleep_time_field_description",
+        wakeTime: "wake_time_field_label",
+        wakeTimeDesc: "wake_time_field_description",
+        neckStiffness: "neck_stiffness_field_label",
+        neckStiffnessDesc: "neck_stiffness_field_description",
+        timeToGetUp: "time_to_get_up_field_label",
+        timeToGetUpDesc: "time_to_get_up_field_description"
       }
     },
     wellbeing: {
       title: "wellbeing_section_title",
       description: "wellbeing_section_description",
       fields: {
-        workMotivation: "work_motivation_field_description",
-        mood: "mood_field_description",
-        socialSatisfaction: "social_satisfaction_field_description",
-        physicalActivityDesire: "physical_activity_desire_field_description",
-        headache: "headache_field_description",
-        sleepiness: "sleepiness_field_description",
-        physicalFatigue: "physical_fatigue_field_description"
+        workMotivation: "work_motivation_field_label",
+        workMotivationDesc: "work_motivation_field_description",
+        mood: "mood_field_label",
+        moodDesc: "mood_field_description",
+        socialSatisfaction: "social_satisfaction_field_label",
+        socialSatisfactionDesc: "social_satisfaction_field_description",
+        physicalActivityDesire: "physical_activity_desire_field_label",
+        physicalActivityDesireDesc: "physical_activity_desire_field_description",
+        headache: "headache_field_label",
+        headacheDesc: "headache_field_description",
+        sleepiness: "sleepiness_field_label",
+        sleepinessDesc: "sleepiness_field_description",
+        physicalFatigue: "physical_fatigue_field_label",
+        physicalFatigueDesc: "physical_fatigue_field_description"
       }
     },
     activityAndDiet: {
       title: "activity_diet_section_title",
       description: "activity_diet_section_description",
       fields: {
-        stepsCount: "steps_count_field_description",
-        fullMealsCount: "full_meals_count_field_description",
-        fruitsVeggiesPortions: "fruits_veggies_portions_field_description",
-        alcoholMl: "alcohol_ml_field_description",
-        sweetsPortions: "sweets_portions_field_description",
-        sweetDrinksPortions: "sweet_drinks_portions_field_description"
+        stepsCount: "steps_count_field_label",
+        stepsCountDesc: "steps_count_field_description",
+        fullMealsCount: "full_meals_count_field_label",
+        fullMealsCountDesc: "full_meals_count_field_description",
+        fruitsVeggiesPortions: "fruits_veggies_portions_field_label",
+        fruitsVeggiesPortionsDesc: "fruits_veggies_portions_field_description",
+        alcoholMl: "alcohol_ml_field_label",
+        alcoholMlDesc: "alcohol_ml_field_description",
+        sweetsPortions: "sweets_portions_field_label",
+        sweetsPortionsDesc: "sweets_portions_field_description",
+        sweetDrinksPortions: "sweet_drinks_portions_field_label",
+        sweetDrinksPortionsDesc: "sweet_drinks_portions_field_description"
       }
     }
   }
--- client/src/lib/types.ts
+++ client/src/lib/types.ts
@@ -24,58 +24,58 @@ export const formSections: FormSection[] = [
     fields: [
       { 
         name: "sleepQuality", 
-        label: "Jakość snu", 
-        description: formCmsKeys.sections.sleepParameters.fields.sleepQuality,
+        label: formCmsKeys.sections.sleepParameters.fields.sleepQuality,
+        description: formCmsKeys.sections.sleepParameters.fields.sleepQualityDesc,
         type: "number", 
         min: 1, 
         max: 100 
       },
       { 
         name: "sleptWithWindowOpen", 
-        label: "Spanie przy otwartym oknie", 
-        description: formCmsKeys.sections.sleepParameters.fields.sleptWithWindowOpen,
+        label: formCmsKeys.sections.sleepParameters.fields.sleptWithWindowOpen,
+        description: formCmsKeys.sections.sleepParameters.fields.sleptWithWindowOpenDesc,
         type: "boolean" 
       },
       { 
         name: "hadGoodDreams", 
-        label: "Przyjemne sny", 
-        description: formCmsKeys.sections.sleepParameters.fields.hadGoodDreams,
+        label: formCmsKeys.sections.sleepParameters.fields.hadGoodDreams,
+        description: formCmsKeys.sections.sleepParameters.fields.hadGoodDreamsDesc,
         type: "boolean" 
       },
       { 
         name: "hadBadDreams", 
-        label: "Koszmary", 
-        description: formCmsKeys.sections.sleepParameters.fields.hadBadDreams,
+        label: formCmsKeys.sections.sleepParameters.fields.hadBadDreams,
+        description: formCmsKeys.sections.sleepParameters.fields.hadBadDreamsDesc,
         type: "boolean" 
       },
       { 
         name: "wokeUpToToilet", 
-        label: "Wybudzenie do toalety", 
-        description: formCmsKeys.sections.sleepParameters.fields.wokeUpToToilet,
+        label: formCmsKeys.sections.sleepParameters.fields.wokeUpToToilet,
+        description: formCmsKeys.sections.sleepParameters.fields.wokeUpToToiletDesc,
         type: "boolean" 
       },
       { 
         name: "sleepTime", 
-        label: "Godzina położenia się spać", 
-        description: formCmsKeys.sections.sleepParameters.fields.sleepTime,
+        label: formCmsKeys.sections.sleepParameters.fields.sleepTime,
+        description: formCmsKeys.sections.sleepParameters.fields.sleepTimeDesc,
         type: "time" 
       },
       { 
         name: "wakeTime", 
-        label: "Godzina wstania", 
-        description: formCmsKeys.sections.sleepParameters.fields.wakeTime,
+        label: formCmsKeys.sections.sleepParameters.fields.wakeTime,
+        description: formCmsKeys.sections.sleepParameters.fields.wakeTimeDesc,
         type: "time" 
       },
       { 
         name: "neckStiffness", 
-        label: "Sztywność karku", 
-        description: formCmsKeys.sections.sleepParameters.fields.neckStiffness,
+        label: formCmsKeys.sections.sleepParameters.fields.neckStiffness,
+        description: formCmsKeys.sections.sleepParameters.fields.neckStiffnessDesc,
         type: "boolean" 
       },
       { 
         name: "timeToGetUp", 
-        label: "Czas do wstania (minuty)", 
-        description: formCmsKeys.sections.sleepParameters.fields.timeToGetUp,
+        label: formCmsKeys.sections.sleepParameters.fields.timeToGetUp,
+        description: formCmsKeys.sections.sleepParameters.fields.timeToGetUpDesc,
         type: "number", 
         min: 0, 
         max: 120 
@@ -88,56 +88,56 @@ export const formSections: FormSection[] = [
     fields: [
       { 
         name: "workMotivation", 
-        label: "Motywacja do pracy", 
-        description: formCmsKeys.sections.wellbeing.fields.workMotivation,
+        label: formCmsKeys.sections.wellbeing.fields.workMotivation,
+        description: formCmsKeys.sections.wellbeing.fields.workMotivationDesc,
         type: "number", 
         min: 0, 
         max: 10 
       },
       { 
         name: "mood", 
-        label: "Nastrój", 
-        description: formCmsKeys.sections.wellbeing.fields.mood,
+        label: formCmsKeys.sections.wellbeing.fields.mood,
+        description: formCmsKeys.sections.wellbeing.fields.moodDesc,
         type: "number", 
         min: 0, 
         max: 10 
       },
       { 
         name: "socialSatisfaction", 
-        label: "Zadowolenie z kontaktów społecznych", 
-        description: formCmsKeys.sections.wellbeing.fields.socialSatisfaction,
+        label: formCmsKeys.sections.wellbeing.fields.socialSatisfaction,
+        description: formCmsKeys.sections.wellbeing.fields.socialSatisfactionDesc,
         type: "number", 
         min: 0, 
         max: 10 
       },
       { 
         name: "physicalActivityDesire", 
-        label: "Chęć do aktywności fizycznej", 
-        description: formCmsKeys.sections.wellbeing.fields.physicalActivityDesire,
+        label: formCmsKeys.sections.wellbeing.fields.physicalActivityDesire,
+        description: formCmsKeys.sections.wellbeing.fields.physicalActivityDesireDesc,
         type: "number", 
         min: 0, 
         max: 10 
       },
       { 
         name: "headache", 
-        label: "Intensywność bólu głowy", 
-        description: formCmsKeys.sections.wellbeing.fields.headache,
+        label: formCmsKeys.sections.wellbeing.fields.headache,
+        description: formCmsKeys.sections.wellbeing.fields.headacheDesc,
         type: "number", 
         min: 0, 
         max: 10 
       },
       { 
         name: "sleepiness", 
-        label: "Senność", 
-        description: formCmsKeys.sections.wellbeing.fields.sleepiness,
+        label: formCmsKeys.sections.wellbeing.fields.sleepiness,
+        description: formCmsKeys.sections.wellbeing.fields.sleepinessDesc,
         type: "number", 
         min: 0, 
         max: 10 
       },
       { 
         name: "physicalFatigue", 
-        label: "Zmęczenie fizyczne", 
-        description: formCmsKeys.sections.wellbeing.fields.physicalFatigue,
+        label: formCmsKeys.sections.wellbeing.fields.physicalFatigue,
+        description: formCmsKeys.sections.wellbeing.fields.physicalFatigueDesc,
         type: "number", 
         min: 0, 
         max: 10 
@@ -150,48 +150,48 @@ export const formSections: FormSection[] = [
     fields: [
       { 
         name: "stepsCount", 
-        label: "Liczba kroków", 
-        description: formCmsKeys.sections.activityAndDiet.fields.stepsCount,
+        label: formCmsKeys.sections.activityAndDiet.fields.stepsCount,
+        description: formCmsKeys.sections.activityAndDiet.fields.stepsCountDesc,
         type: "number", 
         min: 0, 
         max: 100000 
       },
       { 
         name: "fullMealsCount", 
-        label: "Liczba pełnych posiłków", 
-        description: formCmsKeys.sections.activityAndDiet.fields.fullMealsCount,
+        label: formCmsKeys.sections.activityAndDiet.fields.fullMealsCount,
+        description: formCmsKeys.sections.activityAndDiet.fields.fullMealsCountDesc,
         type: "number", 
         min: 0, 
         max: 5 
       },
       { 
         name: "fruitsVeggiesPortions", 
-        label: "Porcje owoców i warzyw", 
-        description: formCmsKeys.sections.activityAndDiet.fields.fruitsVeggiesPortions,
+        label: formCmsKeys.sections.activityAndDiet.fields.fruitsVeggiesPortions,
+        description: formCmsKeys.sections.activityAndDiet.fields.fruitsVeggiesPortionsDesc,
         type: "number", 
         min: 0, 
         max: 5 
       },
       { 
         name: "alcoholMl", 
-        label: "Spożycie alkoholu (ml)", 
-        description: formCmsKeys.sections.activityAndDiet.fields.alcoholMl,
+        label: formCmsKeys.sections.activityAndDiet.fields.alcoholMl,
+        description: formCmsKeys.sections.activityAndDiet.fields.alcoholMlDesc,
         type: "number", 
         min: 0, 
         max: 500 
       },
       { 
         name: "sweetsPortions", 
-        label: "Porcje słodyczy", 
-        description: formCmsKeys.sections.activityAndDiet.fields.sweetsPortions,
+        label: formCmsKeys.sections.activityAndDiet.fields.sweetsPortions,
+        description: formCmsKeys.sections.activityAndDiet.fields.sweetsPortionsDesc,
         type: "number", 
         min: 0, 
         max: 5 
       },
       { 
         name: "sweetDrinksPortions", 
-        label: "Porcje słodkich napojów", 
-        description: formCmsKeys.sections.activityAndDiet.fields.sweetDrinksPortions,
+        label: formCmsKeys.sections.activityAndDiet.fields.sweetDrinksPortions,
+        description: formCmsKeys.sections.activityAndDiet.fields.sweetDrinksPortionsDesc,
         type: "number", 
         min: 0, 
         max: 10 
